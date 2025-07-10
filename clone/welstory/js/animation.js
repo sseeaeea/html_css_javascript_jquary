e1dd745b2e6a40a7940955442f269388832f4a94
sseeaeea <130345807+sseeaeea@users.noreply.github.com>
2025-07-10 11:30:54 +0900

    finish
    .

--- clone/welstory/js/animation.js
+++ clone/welstory/js/animation.js
@@ -22,18 +22,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
           countStarted1 = true;
 
           $(".num_count").each(function () {
-            const $this = $(this);
-            const target = parseInt($this.data("count-target"));
-            const delay = parseInt($this.data("count-delay"));
+            const this_num1 = $(this);
+            const target = parseInt(this_num1.data("count-target"));
+            const delay = parseInt(this_num1.data("count-delay"));
             let count = 0;
-            const duration = 1000; // 총 시간 (1초)
+            const duration = 1000;
             const step = Math.floor(duration / target);
 
             setTimeout(function () {
               const counter = setInterval(function () {
                 if (count < target) {
                   count++;
-                  $this.text(count);
+                  this_num1.text(count);
                 } else {
                   clearInterval(counter);
                 }
@@ -52,9 +52,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
           countStarted2 = true;
 
           $(".num_count2").each(function () {
-            const $this = $(this);
-            const target = parseInt($this.data("count-target"));
-            const delay = parseInt($this.data("count-delay"));
+            const this_num2 = $(this);
+            const target = parseInt(this_num2.data("count-target"));
+            const delay = parseInt(this_num2.data("count-delay"));
             let count = 0;
             const step = 1;
 
@@ -62,9 +62,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
               const counter = setInterval(function () {
                 if (count < target) {
                   count = count + 40;
-                  $this.text(count);
+                  this_num2.text(count);
                 } else if (count === 11000) {
-                  $this.text("11,000");
+                  this_num2.text("11,000");
                 } else {
                   clearInterval(counter);
                 }
@@ -73,5 +73,139 @@ document.addEventListener("DOMContentLoaded", (event) => {
           });
         }
       },
+    });
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#sec3",
+        start: "top 80%",
+        end: "top 70%",
+        onEnter: () => {
+          $(".sec3_img").addClass("sec3_img_running");
+        },
+      },
+    })
+    .to(".sec3_left_box", {
+      x: 200,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".box1", {
+      x: -200,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".box2", {
+      x: -200,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".box3", {
+      x: -200,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".box4", {
+      x: -200,
+      opacity: 1,
+      ease: "linear",
+    });
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#sec4",
+        start: "top 80%",
+        end: "top 70%",
+        onEnter: () => {
+          // 첫 번째 줄만 먼저 실행
+          $(".sec4_up_con > a").css("background-size", "0% 1px, 100% 1px");
+          $(".sec4_down_con > a").css("background-size", "100% 1px");
+
+          // 두 번째 줄 (top 선)은 500ms 후에 실행
+          setTimeout(() => {
+            $(".sec4_up_con > a").css("background-size", "100% 1px, 100% 1px");
+          }, 500);
+        },
+      },
+    })
+    .to(".sec4_container > h2", {
+      y: -100,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".sec4_up_con", {
+      y: -100,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".sec4_down_con", {
+      y: -100,
+      opacity: 1,
+      ease: "linear",
+    });
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#sec5",
+        start: "top 80%",
+        end: "top 70%",
+        onEnter: () => {
+          $(".sec5_img").addClass("sec5_img_running");
+        },
+      },
+    })
+    .to(".sec5_left_box", {
+      x: 200,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".box5", {
+      x: -200,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".box6", {
+      x: -200,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".box7", {
+      x: -200,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".box8", {
+      x: -200,
+      opacity: 1,
+      ease: "linear",
+    });
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#sec6",
+        start: "top 80%",
+        end: "top 70%",
+        onEnter: () => {
+          $("#sec6_right_con_border1 > a").css("background-size", "100% 1px");
+          $("#sec6_right_con_border2 > a").css("background-size", "100% 1px");
+          $("#sec6_right_con_border3 > a").css("background-size", "100% 1px");
+        },
+      },
+    })
+    .to(".sec4_container > h2", {
+      y: -100,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".sec4_up_con", {
+      y: -100,
+      opacity: 1,
+      ease: "linear",
+    })
+    .to(".sec4_down_con", {
+      y: -100,
+      opacity: 1,
+      ease: "linear",
     });
 });
--- clone/welstory/js/welstory.js
+++ clone/welstory/js/welstory.js
@@ -1,34 +1,56 @@
 //swiper_next_prev_btn
 $(".swiper-button-next").on("click", function () {
-  const $circle = $(".circle");
-  $circle.css("display", "block");
-  $circle.removeClass("animation");
-  void $circle[0].offsetWidth;
-  $circle.addClass("animation");
+  const circle = $(".circle");
+  circle.css("display", "block");
+  circle.removeClass("animation");
+  void circle[0].offsetWidth;
+  circle.addClass("animation");
 
-  const $swiper_text = $(".swiper_text");
-  $swiper_text.css("display", "none");
-  $swiper_text.removeClass("circle_text");
-  void $swiper_text[0].offsetWidth;
-  $swiper_text.css("display", "block");
-  $swiper_text > h2.css("top", "block");
-  $swiper_text > p.css("display", "block");
-  $swiper_text.addClass("circle_text");
+  const swiper_text = $(".swiper_text");
+  swiper_text.css("display", "none");
+  swiper_text.removeClass("circle_text");
+  void swiper_text[0].offsetWidth;
+  swiper_text.css("display", "block");
+  swiper_text > h2.css("top", "block");
+  swiper_text > p.css("display", "block");
+  swiper_text.addClass("circle_text");
 });
 $(".swiper-button-prev").on("click", function () {
-  const $circle = $(".circle");
-  $circle.css("display", "block");
-  $circle.removeClass("animation");
-  void $circle[0].offsetWidth;
-  $circle.addClass("animation");
+  const circle = $(".circle");
+  circle.css("display", "block");
+  circle.removeClass("animation");
+  void circle[0].offsetWidth;
+  circle.addClass("animation");
   $("#circle_text").css("display", "block");
 
-  const $swiper_text = $(".swiper_text");
-  $swiper_text.css("display", "none");
-  $swiper_text.removeClass("circle_text");
-  void $swiper_text[0].offsetWidth;
-  $swiper_text.css("display", "block");
-  $swiper_text > h2.css("top", "block");
-  $swiper_text > p.css("display", "block");
-  $swiper_text.addClass("circle_text");
+  const swiper_text = $(".swiper_text");
+  swiper_text.css("display", "none");
+  swiper_text.removeClass("circle_text");
+  void swiper_text[0].offsetWidth;
+  swiper_text.css("display", "block");
+  swiper_text > h2.css("top", "block");
+  swiper_text > p.css("display", "block");
+  swiper_text.addClass("circle_text");
+});
+
+// sec3 hover
+
+$(".sec3_left_box").on("mouseenter", function () {
+  $(".circle_word3").css("display", "none");
+  $(".circle_word4").css("display", "none");
+});
+$(".sec3_left_box").on("mouseleave", function () {
+  $(".circle_word3").css("display", "block");
+  $(".circle_word4").css("display", "block");
+});
+
+// sec5 hover
+
+$(".sec5_left_box").on("mouseenter", function () {
+  $(".circle_word5").css("display", "none");
+  $(".circle_word6").css("display", "none");
+});
+$(".sec5_left_box").on("mouseleave", function () {
+  $(".circle_word5").css("display", "block");
+  $(".circle_word6").css("display", "block");
 });
